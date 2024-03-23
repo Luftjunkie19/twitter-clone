545b31bb22cad474549db6434003e577292aea6d
Łukasz Borkowski <krawczyk0606@gmail.com>
2024-03-23 16:28:38 +0000

    Final production commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
-import { Inter } from 'next/font/google';
-
 import Form from '@/components/Form';
 import Header from '@/components/layoutComponents/Header';
 import PostFeed from '@/components/posts/PostFeed';
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
